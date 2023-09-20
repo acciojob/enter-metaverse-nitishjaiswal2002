44f4ebaff0738b0f0d5f4fbf3c25db1cbc4bad86
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-20 14:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const button =document.getElementById("enterBtn");
 const para= document.getElementById("status");
 
 button.addEventListener("click" , () => {
-para.innerHTML = "<h1>Entered Metaverse</h1>";
+	let h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+	document.body.insertBefore(h1, para)
+para.remove();
 	
-});
+})
